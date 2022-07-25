4ccc1e5160ea433bb0470b8d54c01205a06e1645
KyungTaeP <108638458+KyungTaeP@users.noreply.github.com>
2022-07-25 16:50:20 +0900

    버전2.2
    버전2.2

--- js/common.js
+++ js/common.js
@@ -1,7 +1,15 @@
+const $logo = document.querySelector('.logo');
 const $menu = document.querySelector('.menu');
 const $ham = document.querySelector('.ham');
 const $bg_size = document.querySelector('.bg_con');
 
+$logo.addEventListener('click', ()=>{
+    // scroll을 top:0까지 부드럽게 올려라(맨 위까지)
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+})
 
 $ham.addEventListener('click', ()=>{
     $menu.classList.toggle('on');
@@ -13,7 +21,8 @@ window.addEventListener('scroll', ()=>{
     let win_y = window.scrollY;
     console.log(win_x, win_y);
 
-    if(win_y >= 245){
+    // scrollY의 값이 245 이상, 1000 이하일 때 커짐
+    if(win_y >= 245 && win_y <= 1000){
         $bg_size.style.width = "calc(100% - 30px)";
         $bg_size.style.height = "calc(100vh - 30px)";
     } else {
